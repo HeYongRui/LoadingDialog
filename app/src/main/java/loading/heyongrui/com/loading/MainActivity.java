23f4c335677be3d79f645b91b4846b4817104dd3
Mr.He <1083010205@qq.com>
2017-03-26 21:05:11 +0800

    .

--- app/src/main/java/loading/heyongrui/com/loading/MainActivity.java
+++ app/src/main/java/loading/heyongrui/com/loading/MainActivity.java
@@ -17,8 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button viewById = (Button) findViewById(R.id.btn);
+        Button viewById2 = (Button) findViewById(R.id.btn2);
+        Button viewById3 = (Button) findViewById(R.id.btn3);
+        Button viewById4 = (Button) findViewById(R.id.btn4);
         loadingDialog = new LoadingDialog(this);
-        loadingDialog.setColor(Color.BLACK);
         loadingDialog.setType(LoadingDialog.FOLDINGCUBE);
         viewById.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +30,32 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     loadingDialog.dismiss();
                 }
+            }
+        });
+        viewById2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadingDialog loadingDialog = new LoadingDialog(MainActivity.this);
+                loadingDialog.setColor(Color.parseColor("#FFE3AB90"));
+                loadingDialog.setType(LoadingDialog.WAVE);
+                loadingDialog.show();
+            }
+        });
+        viewById3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadingDialog loadingDialog = new LoadingDialog(MainActivity.this);
+                loadingDialog.setColor(Color.parseColor("#FFE38888"));
+                loadingDialog.setType(LoadingDialog.DOUBLEBOUNCE);
+                loadingDialog.show();
+            }
+        });
+        viewById4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadingDialog loadingDialog = new LoadingDialog(MainActivity.this);
+                loadingDialog.setColor(Color.parseColor("#FFA3FF78"));
+                loadingDialog.show();
             }
         });
     }
